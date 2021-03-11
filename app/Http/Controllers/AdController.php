f9da46dbd596cc6841f43dafdf0de1d509cbaf10
EC2 Default User <ec2-user@ip-172-31-36-57.us-east-2.compute.internal>
2021-03-11 23:15:20 +0000

    m

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,8 +10,8 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://a-trade.jp/redirect/connect?media=G16531'); //CONNECT - Aトレード
+            return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=17545'); //恋バナナ - fam
         }
-        return redirect('https://a-trade.jp/redirect/connect?media=G16531'); //CONNECT - Aトレード
+        return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=17545'); //恋バナナ - fam
     }
 }
