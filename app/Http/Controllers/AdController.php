31b4beef13f23e7d1389df11b6380ea3dfdc8164
EC2 Default User <ec2-user@ip-172-31-36-57.us-east-2.compute.internal>
2021-03-17 23:15:51 +0000

    m

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -10,8 +10,10 @@ class AdController extends Controller
     public function os(Request $request) {
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
-            return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=17545'); //恋バナナ - fam
+            return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=18025'); //コンプレックスラブ - fam
+            // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=17545'); //恋バナナ - fam
         }
-        return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=17545'); //恋バナナ - fam
+        return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=18025'); //コンプレックスラブ - fam
+        // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=17545'); //恋バナナ - fam
     }
 }
